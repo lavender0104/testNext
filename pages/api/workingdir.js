1049310e57bd9e4962498458a9e81a1fccc01503
lavender0104 <73227747+lavender0104@users.noreply.github.com>
2020-12-03 23:24:53 +0800

    Update workingdir.js

--- pages/api/workingdir.js
+++ pages/api/workingdir.js
@@ -2,5 +2,6 @@ export default (req, res) => {
   res.status(200).json({ name: "next.js" });
   //const { total_price, currency } = req; 
   //console.log(currency, total_price);
-  console.log(req.query, req.body);
+  //console.log(req.query, req.body);
+  console.log(req);
 };

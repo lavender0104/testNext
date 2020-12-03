8dcadc0d0ea2c54d6c951380cd956ef8a75bb1f4
lavender0104 <73227747+lavender0104@users.noreply.github.com>
2020-12-04 00:32:01 +0800

    Update workingdir.js

--- pages/api/workingdir.js
+++ pages/api/workingdir.js
@@ -2,5 +2,5 @@ export default (req, res) => {
   res.status(200).json({ name: "next.js" });
   //const { product_id, id, title } = req.body;
   //console.log(product_id, id, title);
-  console.log(req.body.grams, req.body.id);
+  console.log(req.body.id);
 };
